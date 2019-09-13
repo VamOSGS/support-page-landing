70088a86d9819e4a8d5ba521941bacac3e4dc7cd
VamOSGS <gegham.samvelyan@hotmail.com>
2019-09-14 03:06:36 +0400

    Header implemented

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 const open = document.querySelectorAll('.open');
+const dropDownOpener = document.querySelectorAll('.dropDownOpener');
 open.forEach(item => {
   item.onclick = e => {
     e.target.parentNode.classList.toggle('active');
   };
 });
+dropDownOpener.onmouseover = (e) => {
+  e.target.parentNode.classList.toggle('active')
+}
