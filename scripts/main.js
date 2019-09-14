0c2bc6d3bf284304d4e19091b410329f88869e25
VamOSGS <gegham.samvelyan@hotmail.com>
2019-09-14 11:49:09 +0400

    Added responsive styles for header

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
 const open = document.querySelectorAll('.open');
 const dropDownOpener = document.querySelectorAll('.dropDownOpener');
+const menuBtn = document.querySelector('.menu-btn');
+const header = document.querySelector('.header');
+
 open.forEach(item => {
   item.onclick = e => {
     e.target.parentNode.classList.toggle('active');
   };
 });
-dropDownOpener.onmouseover = (e) => {
-  e.target.parentNode.classList.toggle('active')
-}
+dropDownOpener.onmouseover = e => {
+  e.target.parentNode.classList.toggle('active');
+};
+
+menuBtn.onclick = () => {
+  header.classList.toggle('opened');
+};
