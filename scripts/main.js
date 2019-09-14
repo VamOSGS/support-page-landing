b94c12a412695e3809605c0efeabd178c1796194
VamOSGS <gegham.samvelyan@hotmail.com>
2019-09-14 14:12:13 +0400

    Implemented header scroll effect

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,30 @@ const open = document.querySelectorAll('.open');
 const dropDownOpener = document.querySelectorAll('.dropDownOpener');
 const menuBtn = document.querySelector('.menu-btn');
 const header = document.querySelector('.header');
+let menu = false;
 
 open.forEach(item => {
   item.onclick = e => {
     e.target.parentNode.classList.toggle('active');
   };
 });
-dropDownOpener.onmouseover = e => {
-  e.target.parentNode.classList.toggle('active');
-};
 
 menuBtn.onclick = () => {
   header.classList.toggle('opened');
+  menu = !menu;
+  document.body.style.overflow = menu ? 'hidden' : 'unset';
 };
+
+dropDownOpener.forEach(item => {
+  item.onclick = e => {
+    e.target.parentNode.classList.toggle('active');
+  };
+});
+
+window.addEventListener('scroll', e => {
+  if (window.pageYOffset === 0) {
+    header.classList.remove('white');
+  } else {
+    header.classList.add('white');
+  }
+});
